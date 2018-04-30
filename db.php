b6b31bd89d0e4c09740d5c0c58fe7fe50cb4d4d5
Thilina Dissanayake <thilinasupunsara@gmail.com>
2018-04-30 20:02:16 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+$dbhost = 'localhost'; 
+$dbuser = 'root'; 
+$dbpass = ''; 
+ 
+$conn = mysql_connect($dbhost, $dbuser, $dbpass) ; 
+if (!$conn)   {   
+    die('Could not connect: ' . mysql_error()); 
+}
+mysql_select_db( "w1626640_0",$conn); 
+?>
